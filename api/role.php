50b5c672dd5c4a1169829f6a58c2fdcb9b7dd51c
Nankap <keshianankap@gmail.com>
2021-12-03 02:52:18 +0100

    ajouter les conditions de garde sur la l'attribution des roles a une personne

--- api/role.php
+++ api/role.php
@@ -35,7 +35,13 @@ switch($routes){
     case 'roleperson':
         $idperson = $_POST['idperson'];
         $idrole = $_POST['idrole'];
-        $createRolePerson = $roleperson->createRoleperson($idperson,$idrole);
+        if(empty($idperson) || empty($idrole)){
+            $retour["error"] = "vous devez preciser la personne et son role";
+        }else{
+            $createRolePerson = $roleperson->createRoleperson($idperson,$idrole);
+            $retour["message"] = "l'attribution de role a l'utilisateur a ete fait avec success.";
+        }
+        echo json_encode($retour);
     break;
 }
 
